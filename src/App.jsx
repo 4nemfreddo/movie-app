09179b36501982bdc2fd142ee3e332b21d377ba4
4nemfreddo <killmyenvy@gmail.com>
2025-03-13 01:51:27 +0300

    updated Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import Search from './components/search.jsx'
+import { useState } from 'react'
 
 const App = () => {
+  const [searchTerm, setSearchTerm] = useState('');
+
   return ( 
     <main>
       <div className="pattern" />
 
-      <div className="wrapper"/>
+      <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
           <h1>Get <span className="text-gradient">Movies</span> For All Moods And Occassions For You and Loved Ones</h1>
         </header>
 
-        <p>Search</p>
-
+        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+      </div>  
     </main>
     )
   }
