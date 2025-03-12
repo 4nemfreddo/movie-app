cea64bda782af6db003e78ba681e5dbf90a96aba
4nemfreddo <killmyenvy@gmail.com>
2025-03-13 01:09:46 +0300

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const App = () => {
+  return ( 
+    <main>
+      <div className="pattern" />
+
+      <div className="wrapper"/>
+        <header>
+          <img src="./hero.png" alt="Hero Banner" />
+          <h1>Get <span className="text-gradient">Movies</span> For All Moods And Occassions For You and Loved Ones</h1>
+        </header>
+
+        <p>Search</p>
+
+    </main>
+    )
+  }
+
+export default App
