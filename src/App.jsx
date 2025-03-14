ec5741b9d6027e26905896687e6df4d101638465
4nemfreddo <killmyenvy@gmail.com>
2025-03-14 03:45:30 +0300

    resolved API fetch issues

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,59 @@
 import React from 'react';
 import Search from './components/Search.jsx';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+
+const API_BASE_URL = 'https://api.themoviebd.org/3';
+
+const API_KEY = import.meta.env.VITE_TMDB_API_KEY;
+
+const API_OPTIONS = {
+  method: 'GET',
+  Headers: {
+    accept: 'application/json',
+    Authorization: `Bearer ${API_KEY}`
+  }
+}  
 
 const App = () => {
   const [searchTerm, setSearchTerm] = useState('');
+  const [errorMessage, setErrorMessage] = useState('')
+  const [movieList, setMovieList] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
+  const fetchMovies = async () => {
+    setIsLoading(true);
+    setErrorMessage('');
+
+    try {
+      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`;
+
+      const response = await fetch(endpoint, API_OPTIONS);
+
+      if(!response.ok) {
+        throw new Error('Failed to fetch movies');
+      }
+
+      const data = await response.json();
+
+      if(data.Response = 'False') {
+        setErrorMessage(data.Error || 'Failed to fetch movies');
+        setMovieList([]);
+        return;
+      }
+
+        setMovieList(data.results || []);
+    } catch (error) {
+      console.error(`Error fetching movies: ${error}`);
+      setErrorMessage('Error fetching movies. Please try again Later.');
+    } finally {
+      setIsLoading(false);
+    }
+  }
+  
+  useEffect(() => {
+    fetchMovies();
+  }, []);
+  
   return ( 
     <main>
       <div className="pattern" />
@@ -12,10 +61,16 @@ const App = () => {
       <div className="wrapper">
         <header>
           <img src="./hero.png" alt="Hero Banner" />
-          <h1>Get Movies For All Your <span className="text-gradient"> Moods And Occassions</span></h1>
+          <h1>Get All Your <span className="text-gradient">Movies</span> In One Place</h1>
+
+          <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
         </header>
 
-        <Search searchTerm={searchTerm} setSearchTerm={setSearchTerm} />
+        <section className="all-movies">
+          <h2>All Movies</h2>
+
+          {errorMessage && <p className="text-red-500">{errorMessage}</p>}
+        </section>
       </div>  
     </main>
     )
